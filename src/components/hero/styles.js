604126c16bb284b6d4241d8e6550c4fc599c1f2d
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-02-09 01:45:15 -0300

    animando o hero

--- src/components/hero/styles.js
+++ src/components/hero/styles.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
+import { TileSize } from "../../settings/constants";
 
 export const Hero = styled.div`
+    animation: hero-animation 1s steps(4) infinite;
     background-image: url('./assets/img/hero.png');
     background-repeat: no-repeat;
     height: 100px;
-    width: 300px;
+    width: ${TileSize}px;
 
     @keyframes hero-animation {
         from{
@@ -12,7 +14,7 @@ export const Hero = styled.div`
         }
         
         to{
-            background-position-x: 192px;
+            background-position-x: -192px;
         }
     }
 `
