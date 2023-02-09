fad30dad9ebf093ac79a3a0ec7aa0c42316e6ef0
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-02-09 00:57:26 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css'
-import Home from './pages/home';
-import Movie from './pages/movie';
-import Search from './pages/search';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route element={<App/>}>
-        <Route path='/' element={<Home/>}/>
-        <Route path='movie/:id' element={<Movie/>}/>
-        <Route path='search' element={<Search/>}/>
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <App />
 );
 
