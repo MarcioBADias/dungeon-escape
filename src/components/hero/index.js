7187e72b937966c004b5a974b452b813758b1edc
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-02-11 22:43:52 -0300

    Ajustes de controle

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -10,24 +10,24 @@ const Hero = () => {
     const [ heroPosition, setHeroPosition ] = useState(positionInicital);
 
     useEffect(() => {
-        const handleKeyUp = (event) => {
-          if(event.key === 'ArrowUp' || event.key === 'w'){
-            const newPosition = { x:4, y:heroPosition.y++ };
-            setHeroPosition(newPosition);
-          }
-          if(event.key === 'ArrowDown' || event.key === 's'){
-            const newPosition = { x:4, y:heroPosition.y-- };
-            setHeroPosition(newPosition);
-          }
-          if(event.key === 'ArrowRight' || event.key === 'd'){
-            const newPosition = { x:heroPosition.x++, y:heroPosition.y };
-            setHeroPosition(newPosition);
-          }
-          if(event.key === 'ArrowLeft' || event.key === 'a'){
-            const newPosition = { x:heroPosition.x--, y:heroPosition.y };
-            setHeroPosition(newPosition);
-          }
-        };
+      const handleKeyUp = (event) => {
+        if(event.key === 'ArrowUp' || event.key === 'w'){
+          const newPosition = { x:heroPosition.x, y:heroPosition.y++ };
+          setHeroPosition(newPosition);
+        }
+        if(event.key === 'ArrowDown' || event.key === 's'){
+          const newPosition = { x:heroPosition.x, y:heroPosition.y-- };
+          setHeroPosition(newPosition);
+        }
+        if(event.key === 'ArrowRight' || event.key === 'd'){
+          const newPosition = { x:heroPosition.x++, y:heroPosition.y };
+          setHeroPosition(newPosition);
+        }
+        if(event.key === 'ArrowLeft' || event.key === 'a'){
+          const newPosition = { x:heroPosition.x--, y:heroPosition.y };
+          setHeroPosition(newPosition);
+        }
+      };
       
         window.addEventListener("keyup", handleKeyUp);
       
