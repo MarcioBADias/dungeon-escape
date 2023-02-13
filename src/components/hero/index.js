feeba03a802701bbabda39c4f2bc67ad3223b324
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-02-13 12:20:01 -0300

    Refatorando e criando hook do movement do hero

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,53 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { TileSize } from "../../settings/constants";
+import HeroMoviment from "../hooks/heroMoviment";
 import * as C from './styles'
 
-const positionInicital = {
-    x:4,
-    y:10
+const initialPosition = {
+  x:4,
+  y:10
 }
+
 const Hero = () => {
-    const [ heroPosition, setHeroPosition ] = useState(positionInicital);
-    const [ heroDirection, setHeroSirection ] = useState('right');
+  const { heroPosition, heroDirection } = HeroMoviment(initialPosition);
 
-    useEffect(() => {
-      const handleKeyUp = e => {
-        if(e.key === 'ArrowUp' || e.key === 'w'){
-          const newPosition = { x:heroPosition.x, y:++heroPosition.y };
-          setHeroPosition(newPosition);
-        }
-        if(e.key === 'ArrowDown' || e.key === 's'){
-          const newPosition = { x:heroPosition.x, y:--heroPosition.y };
-          setHeroPosition(newPosition);
-        }
-        if(e.key === 'ArrowRight' || e.key === 'd'){
-          const newPosition = { x:++heroPosition.x, y:heroPosition.y };
-          setHeroPosition(newPosition);
-          setHeroSirection('right');
-        }
-        if(e.key === 'ArrowLeft' || e.key === 'a'){
-          const newPosition = { x:--heroPosition.x, y:heroPosition.y };
-          setHeroPosition(newPosition);
-          setHeroSirection('left')
-        }
-      };
-      
-        window.addEventListener("keyup", handleKeyUp);
-      
-        return () => {
-          window.removeEventListener("keyup", handleKeyUp);
-        };
-    }, [heroPosition.x, heroPosition.y]);
-      
-
-    return (
-        <C.Hero
-        style={{
-            bottom: `${TileSize*heroPosition.y}px`,
-            left: `${TileSize*heroPosition.x}px`,
-            transform: `scaleX(${heroDirection === 'right'? 1 : -1})`
-        }}/>
-    )
+  return (
+      <C.Hero
+      style={{
+          bottom: `${TileSize*heroPosition.y}px`,
+          left: `${TileSize*heroPosition.x}px`,
+          transform: `scaleX(${heroDirection === 'right'? 1 : -1})`
+      }}/>
+  )
 }
 
 export default Hero;
---
+++ src/components/hooks/heroMoviment.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+const HeroMoviment = (initialPosition) => {
+    const [ heroPosition, setHeroPosition ] = useState(initialPosition);
+    const [ heroDirection, setHeroDirection ] = useState('right');
+
+    useEffect(() => {
+        const handleKeyUp = e => {
+            const moviments = {
+                ArrowUp: { x:heroPosition.x, y:heroPosition.y +1 },
+                w: { x:heroPosition.x, y:heroPosition.y +1 },
+                ArrowDown: { x:heroPosition.x, y:heroPosition.y -1 },
+                s: { x:heroPosition.x, y:heroPosition.y -1 },
+                ArrowRight: { x:heroPosition.x +1, y:heroPosition.y },
+                d: { x:heroPosition.x +1, y:heroPosition.y },
+                ArrowLeft: { x:heroPosition.x -1, y:heroPosition.y },
+                a: { x:heroPosition.x -1, y:heroPosition.y }
+            }
+            const key = e.key;
+          
+            if(moviments.hasOwnProperty(key)) {
+                const newPosition = moviments[key];
+                setHeroPosition(newPosition);
+                if(key === 'ArrowRight' || key === 'd'){
+                    setHeroDirection('right');
+                }else if(key === 'ArrowLeft' || key === 'a'){
+                    setHeroDirection('left');
+                }
+            }
+        };
+
+        window.addEventListener('keyup', handleKeyUp);
+        
+        return () => {
+            window.removeEventListener('keyup', handleKeyUp);
+        };
+    }, [heroPosition]);
+
+    return { heroPosition, heroDirection }
+}
+
+export default HeroMoviment;
