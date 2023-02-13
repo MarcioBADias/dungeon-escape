f95435ec41e317886fab6adb8638b331a245040a
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-02-13 15:33:17 -0300

    ajustando  log do bilding de deploy

--- src/components/hooks/monsterMoviment.js
+++ src/components/hooks/monsterMoviment.js
@@ -2,42 +2,44 @@ import { useEffect, useState } from "react";
 
 const MonsterMoviment = (initialPosition, moveTime) => {
     const [ monsterPosition, setMonsterPosition ] = useState(initialPosition);
-    const [ monsterDirection, setMonsterDirection ] = useState('right');
+    const [ monsterDirection, setMonsterDirection ] = useState('right'); 
+    
     const randomIndex = Math.floor(Math.random()*4);
 
-    const directions = { 
-        left: 'left', 
-        right: 'right', 
-        up: 'up', 
-        down: 'down' 
-    }; 
-    
     useEffect(() => {
-        const interval = setInterval(() => {
-                const moviments = {
-                    up: { x:monsterPosition.x, y:monsterPosition.y +1 },
-                    down: { x:monsterPosition.x, y:monsterPosition.y -1 },
-                    right: { x:monsterPosition.x +1, y:monsterPosition.y },
-                    left: { x:monsterPosition.x -1, y:monsterPosition.y }
-                }
-                const moveDirection = Object.keys(directions)[randomIndex];
+
+        const directions = { 
+            left: 'left', 
+            right: 'right', 
+            up: 'up', 
+            down: 'down' 
+        };
+
+        const monsterMove = setInterval(() => {
+            const moviments = {
+                up: { x:monsterPosition.x, y:monsterPosition.y +1 },
+                down: { x:monsterPosition.x, y:monsterPosition.y -1 },
+                right: { x:monsterPosition.x +1, y:monsterPosition.y },
+                left: { x:monsterPosition.x -1, y:monsterPosition.y }
+            }
+            const moveDirection = Object.keys(directions)[randomIndex];
               
-                if(moviments.hasOwnProperty(moveDirection)) {
-                    const newPosition = moviments[moveDirection];
-                    setMonsterPosition(newPosition);
-                    if(moveDirection === 'right'){
-                        setMonsterDirection('right');
-                    }else if(moveDirection === 'left'){
-                        setMonsterDirection('left');
-                    }
+            if(moviments.hasOwnProperty(moveDirection)) {
+                const newPosition = moviments[moveDirection];
+                setMonsterPosition(newPosition);
+                if(moveDirection === 'right'){
+                    setMonsterDirection('right');
+                }else if(moveDirection === 'left'){
+                    setMonsterDirection('left');
                 }
             }
-        ,moveTime*1000);
+        }
+    ,moveTime*1000);
         
-        return () => clearInterval(interval);
-    }, [monsterPosition, directions, moveTime, randomIndex]);
+    return () => clearInterval(monsterMove);
+}, [monsterPosition, moveTime, randomIndex]);
 
-    return { monsterPosition, monsterDirection }
+return { monsterPosition, monsterDirection }
 }
 
 export default MonsterMoviment;
